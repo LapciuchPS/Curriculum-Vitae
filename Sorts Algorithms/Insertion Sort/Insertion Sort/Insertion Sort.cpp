2e0f993e1ea4c52db254fdcdc91e8e1af2646796
Andrzej Fijalkowski <andrew.f@interia.eu>
2025-05-31 17:10:51 +0200

    Implemented insertion sort

---
+++ Sorts Algorithms/Insertion Sort/Insertion Sort/Insertion Sort.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <vector>
+
+template <typename T>
+void InsertionSort(std::vector<T>& container);
+
+int main()
+{
+    std::vector<float> numbers{ 23,43,21,4,6,2,7,1};
+
+    InsertionSort(numbers);
+
+    for (auto& elem : numbers)
+        std::cout << elem << " ";
+}
+
+template <typename T>
+void InsertionSort(std::vector<T>& container)
+{
+    if (container.size() < 2) return;
+
+   for(size_t starting_index = 1; starting_index <container.size(); starting_index++)
+   {
+       for (size_t curr_index = starting_index; curr_index > 0; curr_index--)
+       {
+           if (container[curr_index] >= container[curr_index - 1])
+               break;
+
+              std::swap(container[curr_index], container[curr_index - 1]);
+       }
+   }
+}
